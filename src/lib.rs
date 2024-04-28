298a4499f23805d5059d4c784f45b253a18b8d56
r4t1n <r4t1n@proton.me>
2024-04-28 17:40:42 +0200

    Split sorting into lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,29 @@
+pub fn sort(input: String) -> String {
+    let empty: &str = "X";
+    let separator: &str = "-";
+
+    let mut input_vec: Vec<&str> = input.split(separator).collect(); // Input
+    let mut index_rev: u32 = input_vec.len() as u32 - 1; // Index from right to left, we subtract by 1 because indexing in Rust starts at 0
+    let mut index: u32 = 0; // Index from left to right
+
+    // Once index_rev == index we finished sorting
+    'outer: while index_rev != index {
+        if input_vec[index_rev as usize] != empty {
+            while input_vec[index as usize] != empty {
+                // We also need this check because index can move past index_rev when sorting
+                if index_rev == index {
+                    break 'outer;
+                };
+
+                index += 1;
+            }
+
+            input_vec.swap(index_rev as usize, index as usize);
+            index += 1;
+        };
+
+        index_rev -= 1;
+    }
+
+    input_vec.join(separator)
+}
